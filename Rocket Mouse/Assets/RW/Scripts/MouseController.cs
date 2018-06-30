4dae28092c6aa3e6a6f5572499d9f6f119dcd74b
NthnyVllflrs <TS201402078@gmail.com>
2018-06-30 06:01:40 -0700

    Committed Rocket Mouse Project

---
+++ Rocket Mouse/Assets/RW/Scripts/MouseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour 
+{
+
+	public float JetPackForce = 75.0f;
+
+	private Rigidbody2D PlayerRigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void FixedUpdate() 
+	{
+		bool jetPackActive = Input.GetButton("Fire1");
+		if(jetPackActive)
+		{
+			PlayerRigidBody.AddForce(new Vector2(0, JetPackForce));
+		}
+	}
+}
